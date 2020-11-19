84fb26b2c01425e777b1706f9d31f91ed2227dbe
avi tshuva <dharmax@gmail.com>
2020-11-19 19:55:00 +0200

    cont

--- src/am-i-allowed.ts
+++ src/am-i-allowed.ts
@@ -9,11 +9,63 @@ interface IPrivilegeManaged {
 }
 
 
-
 export type PermissionChecker = (actor: IActor, operation: Operation, entity: IPrivilegeManaged, specialContext?: any) => Promise<boolean>
 
 export type Operation = string
 
+
+const OperationsTree = {
+    Admin: {
+        AddAdmin: {},
+        DeleteDatabase: {
+            ManageDatabase: {
+                ManageUsers: {
+                    SendMessage: {},
+                }
+            }
+        },
+        Manage: {
+            PowerUser: {
+                Execute: {
+                    GenericAction: {}
+                },
+                Join: {},
+                Disable: {
+                    Suspend: {
+                        Warn: {},
+                        Flag: {}
+                    }
+                },
+                Delete: {
+                    EditAll: {
+                        WriteAll: {
+                            ReadAll: {
+                                ReadDeep: {
+                                    ReadShallow: {
+                                        ReadHeadline: {}
+                                    },
+                                },
+                            }
+                        },
+                        AddStuff: {
+                            Comment: {},
+                            Rate: {
+                                DownVote: {
+                                    UpVote: {}
+                                }
+                            },
+                            DetachItem: {
+                                AttachItem: {}
+                            }
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
+
+const operationsDictionary =
 
 export async function checkPermissionSoft(actor: IActor, operation: Operation, entity: IPrivilegeManaged, specialContext?: any): Promise<boolean> {
 
@@ -46,7 +98,12 @@ export async function checkPermission(actor: IActor, operation: Operation, entit
 
 export async function standardPermissionChecker(actor: IActor, operation: Operation, entity: IPrivilegeManaged, specialContext?: any): Promise<boolean> {
 
+    const operations = operationTree.expandOperation(operation);
 
+    const actorRoles = await getUserRoles( actor,entity)
+
+
+    let hasPermission = await privilegeStorage.queryPermissions(actor, entity, operations)
 
 }
 
@@ -56,3 +113,40 @@ async function globalPermissionCheck(actor: IActor, operation: Operation, specia
     return false;
 }
 
+
+class OperationTree  {
+
+    private parentsMap: { [operationName:string]: string[]} = {}
+
+    constructor(private tree:object) {
+
+        this.processTree(tree)
+    }
+
+    private processTree(tree: object,parent?:object) {
+
+        for( let [key,value] of Object.entries(tree)) {
+            let entry = this.parentsMap[key]
+            if ( !entry)
+                entry = []
+            parent?.
+        }
+
+    }
+
+    /**
+     * expand to include the super-operations
+     * @param operation
+     */
+    expandOperation(operation): Operation[] {
+
+        const parents = operation.getParents()
+        if (!parents.length)
+            return []
+        return [...parents,
+            ...parents.reduce((a, c) => {
+                a.push(...operationTree.expandOperation(c))
+                return a
+            }, [])]
+    }
+}
