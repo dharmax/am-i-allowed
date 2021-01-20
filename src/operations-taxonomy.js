690035605c09a0185832a73fd87989086cb52eca
avi tshuva <dharmax@gmail.com>
2021-01-20 17:12:08 +0200

    enhancement: PermissionsMetaData can receive arrays

---
+++ src/operations-taxonomy.js
@@ -0,0 +1,86 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DefaultOperationsTaxonomy = void 0;
+/**
+ * This structure defines the operations taxonomy. When a permission for a specific operation is given, it implicitly
+ * denotes that its child operations are also permitted. The purpose of such a taxonomy is to save redundant coding and
+ * confusing/illogical permission definitions.
+ * The tree can be altered @see PrivilegeManager constructor
+ */
+exports.DefaultOperationsTaxonomy = {
+    Admin: {
+        AddAdmin: {
+            ChangePermissions: {}
+        },
+        DeleteDatabase: {
+            ManageDatabase: {
+                ManageUsers: {
+                    SendMessage: {},
+                }
+            }
+        },
+        Manage: {
+            PowerUser: {
+                Execute: {
+                    GenericAction: {},
+                    Trade: {
+                        AcceptPayment: {
+                            Sell: {
+                                Loan: {},
+                                Rent: {}
+                            },
+                        },
+                        Buy: {
+                            Lease: {},
+                            Pay: {},
+                            Order: {}
+                        },
+                    },
+                },
+                Eject: {
+                    Invite: {
+                        Join: {
+                            Leave: {}
+                        },
+                    }
+                },
+                Disable: {
+                    Ban: {
+                        Suspend: {
+                            Warn: {},
+                            Flag: {}
+                        }
+                    }
+                },
+                Delete: {
+                    EditAnything: {
+                        WriteAnything: {
+                            WriteCommon: {
+                                ReadCommon: {}
+                            },
+                            ReadAnything: {
+                                ReadDeep: {
+                                    ReadCommon: {
+                                        ReadHeadline: {}
+                                    },
+                                }
+                            },
+                        },
+                        AddStuff: {
+                            Comment: {},
+                            Rate: {
+                                DownVote: {
+                                    UpVote: {}
+                                }
+                            },
+                            DetachItem: {
+                                AttachItem: {}
+                            }
+                        }
+                    }
+                },
+            }
+        }
+    }
+};
+//# sourceMappingURL=operations-taxonomy.js.map
